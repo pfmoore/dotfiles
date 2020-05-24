4b2aabd5884340c26694e5b9c913d68db5e7ffc7
Paul Moore <p.f.moore@gmail.com>
2020-05-24 16:07:11 +0100

    Add a script to set up a new machine

---
+++ setup.ps1
@@ -0,0 +1,80 @@
+# Set basic configuration variables
+[Environmnet]::SetEnvironmentVariable("WORK_DIRECTORY", "C:\Work\Scratch", "User")
+
+# PATH should have
+#   ~/.local/scripts
+#   ~/.local/bin
+# in that order
+
+#========================================
+# $paths = (
+#     [Environment]::GetEnvironmentVariable("PATH", "User") -split ";" |
+#     Where-Object { $_ -notlike "$env:USERPROFILE\.local\*" }
+# ) -join ";"
+#
+# $local = (
+#     ("scripts", "bin") | Foreach-Object { Join-Path $env:USERPROFILE ".local" $_ }
+# ) -join ";"
+#
+# [Environment]::SetEnvironmentVariable("PATH", "$local;$paths", "User")
+#========================================
+
+# PATHEXT should have .PY and .PYZ
+
+# Install our preferred Powershell modules
+
+Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+Install-Package Posh-Git
+Install-Package Use-RawPipeline
+
+# We expect the following to be installed:
+#   - Python
+#   - Cygwin
+
+# Install scoop
+
+Invoke-WebRequest -UseBasicParsing get.scoop.sh | Invoke-Expression
+scoop update
+scoop install git
+scoop bucket add enk https://github.com/pfmoore/scoop-enk
+scoop bucket add pipx https://github.com/uranusjr/pipx-standalone
+
+scoop install 7zip
+scoop install bat
+scoop install curl
+scoop install fd
+scoop install fzf
+scoop install goawk
+scoop install graphviz
+scoop install jq
+scoop install just
+scoop install monaco
+scoop install pandoc
+scoop install pup
+scoop install ripgrep
+scoop install spigot
+scoop install sqlite
+scoop install wget
+scoop install wkhtmltopdf
+scoop install xsv
+
+scoop install keypirinha
+scoop install digraphmgr
+scoop install cyg
+scoop install volt
+scoop install vim
+
+scoop install pipx
+
+pipx install black
+pipx install flake8
+pipx install howdoi
+pipx install invoke
+pipx install mypy
+pipx install nox
+pipx install pew
+pipx install pip-run
+pipx install pipenv
+pipx install shiv
+pipx install tox
+pipx install virtualenv
