dda552f4b13a41c3120fd3e0aaa32b15292ec74e
Paul Moore <p.f.moore@gmail.com>
2021-01-29 10:27:50 +0000

    Finish gh-event.py

--- dotfiles/.local/scripts/gh-event.py
+++ dotfiles/.local/scripts/gh-event.py
@@ -1,6 +1,30 @@
 import requests
 import subprocess
 from urllib.parse import urlparse
+from argparse import ArgumentParser
+import json
+
+p = ArgumentParser()
+p.add_argument("--payload-json", help="The complete client payload, in JSON format")
+p.add_argument("type", help="The event type")
+p.add_argument("payload", nargs="*", help="Payload values, in the form NAME=VALUE")
+args = p.parse_args()
+
+payload = {}
+if args.payload_json:
+    if args.payload:
+        raise SystemError("Cannot specify both key/value and JSON payload values")
+    payload = json.loads(args.payload_json)
+elif args.payload:
+    for item in args.payload:
+        key, sep, val = item.partition("=")
+        if sep != "=":
+            raise SystemError(f"Payload values must have the form KEY=VALUE: {item}")
+        payload[key] = val
+
+data = { "event_type": args.type }
+if payload:
+    data["client_payload"] = payload
 
 origin = subprocess.run(
     ["git", "remote", "get-url", "origin"],
@@ -20,7 +44,6 @@ cred = subprocess.run(
 credentials = dict(line.split("=") for line in cred.splitlines())
 token = credentials["password"]
 
-data = { "event_type": "release" }
 
 headers = {
     "Accept": "application/vnd.github.everest-preview+json",
